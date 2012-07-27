23292a87a832279602977498fec9f266f261a8b1
Alban Tiberghien <alban.tiberghien@gmail.com>
2012-07-27 20:05:51 +0200

    Askbot context processor available for whole site

--- askbot/context.py
+++ askbot/context.py
@@ -13,16 +13,6 @@ from askbot.utils import url_utils
 
 def application_settings(request):
     """The context processor function"""
-    #FIXME: @atiberghien : temporary, in order to allow language prefix in URL
-    if "/%s" % settings.ASKBOT_URL not in request.path:
-        #todo: this is a really ugly hack, will only work
-        #when askbot is installed not at the home page.
-        #this will not work for the
-        #heavy modders of askbot, because their custom pages
-        #will not receive the askbot settings in the context
-        #to solve this properly we should probably explicitly
-        #add settings to the context per page
-        return {}
     my_settings = askbot_settings.as_dict()
     my_settings['LANGUAGE_CODE'] = getattr(request, 'LANGUAGE_CODE', settings.LANGUAGE_CODE)
     my_settings['ASKBOT_URL'] = settings.ASKBOT_URL
